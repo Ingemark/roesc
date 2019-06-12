43931839181ec45e0b6e3bb8112428f317d6e1bd
Josip Gracin <josip.gracin@gmail.com>
2019-06-12 15:28:03 +0200

    updated Clojure version

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "RoomOrders Escalation System"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1-beta2"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "0.4.490"]
                  [com.cognitect/http-client "0.1.87"]
                  [com.cognitect.aws/api "0.8.289"]
